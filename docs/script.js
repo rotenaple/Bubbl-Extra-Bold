0fa26f9d44cefbd285c1ac10d4ccd50867350527
Edward <rotenaple@protonmail.com>
2025-08-31 20:48:24 +0930

    Update glyphs

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const randomColorButton = document.getElementById('randomColorButton');
 
     // Define the font version
-    var fontVersion = '24w07a';
+    var fontVersion = '25w36a';
 
     // Dynamically create the @font-face rule and append it to the document
     var fontFaceStyle = document.createElement('style');
